4361c3c525df89be78e8eaaef842a2b100040056
Alexis Carrère-Moreno <alcamoru@icloud.com>
2023-10-27 13:25:24 +0200

    Finished translating xaml in code-behind in the historic of matches

--- NexusClient/SummonerInfoPage.xaml.cs
+++ NexusClient/SummonerInfoPage.xaml.cs
@@ -178,19 +178,203 @@ public sealed partial class SummonerInfoPage : Page
                     Grid.SetColumn(kdaChampionTextBlock, 0);
                     Grid.SetRow(kdaChampionTextBlock, 1);
                     Grid.SetColumnSpan(kdaChampionTextBlock, 2);
+                    int teamKills = 1;
+                    foreach (Team team in match.Info.Teams)
+                    {
+                        if (team.TeamId == participant.TeamId)
+                        {
+                            teamKills = team.Objectives.Champion.Kills;
+                        }
+                    }
 
                     TextBlock kpChampionTextBlock = new TextBlock()
                     {
                         TextAlignment = TextAlignment.Center,
-                        Text = $"{participant.Kills} | {participant.Deaths} | {participant.Assists}",
+                        Text = $"{(participant.Kills + participant.Assists) / teamKills}",
                         FontSize = 20,
                         FontFamily = new FontFamily("Assets/fonts/Inter/Inter-Medium.ttf#Inter")
                     };
 
-                    Grid.SetColumn(kdaChampionTextBlock, 0);
+                    Grid.SetColumn(kdaChampionTextBlock, 2);
                     Grid.SetRow(kdaChampionTextBlock, 1);
                     Grid.SetColumnSpan(kdaChampionTextBlock, 2);
 
+                    TextBlock csChampionTextBlock = new TextBlock()
+                    {
+                        TextAlignment = TextAlignment.Center,
+                        Text = $"{participant.TotalMinionsKilled}",
+                        FontSize = 20,
+                        FontFamily = new FontFamily("Assets/fonts/Inter/Inter-Medium.ttf#Inter")
+                    };
+
+                    Grid.SetColumn(kdaChampionTextBlock, 4);
+                    Grid.SetRow(kdaChampionTextBlock, 1);
+                    Grid.SetColumnSpan(kdaChampionTextBlock, 2);
+
+
+                    StackPanel summonersStackPanel = new StackPanel();
+                    TextBlock visionChampionTextBlock = new TextBlock()
+                    {
+                        TextAlignment = TextAlignment.Center,
+                        Text = $"{participant.VisionScore}",
+                        FontSize = 20,
+                        FontFamily = new FontFamily("Assets/fonts/Inter/Inter-Medium.ttf#Inter")
+                    };
+
+                    Grid summonerChampionGrid = new Grid();
+
+                    ColumnDefinition summonerChampionGridColumn1 = new ColumnDefinition()
+                        { Width = new GridLength(30, GridUnitType.Pixel) };
+                    ColumnDefinition summonerChampionGridColumn2 = new ColumnDefinition()
+                        { Width = new GridLength(30, GridUnitType.Pixel) };
+
+                    summonerChampionGrid.ColumnDefinitions.Add(summonerChampionGridColumn1);
+                    summonerChampionGrid.ColumnDefinitions.Add(summonerChampionGridColumn2);
+
+                    RowDefinition summonerChampionGridRow1 = new RowDefinition()
+                        { Height = new GridLength(30, GridUnitType.Pixel) };
+                    RowDefinition summonerChampionGridRow2 = new RowDefinition()
+                        { Height = new GridLength(30, GridUnitType.Pixel) };
+
+                    summonerChampionGrid.RowDefinitions.Add(summonerChampionGridRow1);
+                    summonerChampionGrid.RowDefinitions.Add(summonerChampionGridRow2);
+
+                    Image firstSummonerSpellImage = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(""))
+                    };
+
+                    Grid.SetColumn(firstSummonerSpellImage, 0);
+                    Grid.SetRow(firstSummonerSpellImage, 0);
+
+                    Image secondSummonerSpellImage = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(""))
+                    };
+
+                    Grid.SetColumn(secondSummonerSpellImage, 0);
+                    Grid.SetRow(secondSummonerSpellImage, 1);
+
+                    Image mainRune = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(""))
+                    };
+
+                    Grid.SetColumn(mainRune, 1);
+                    Grid.SetRow(mainRune, 0);
+
+                    Image secondaryRune = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(""))
+                    };
+
+                    Grid.SetColumn(secondaryRune, 1);
+                    Grid.SetRow(secondaryRune, 1);
+
+
+                    summonersStackPanel.Children.Add(visionChampionTextBlock);
+                    summonersStackPanel.Children.Add(summonerChampionGrid);
+
+                    Grid.SetColumn(summonersStackPanel, 0);
+                    Grid.SetRow(summonersStackPanel, 2);
+                    Grid.SetColumnSpan(summonersStackPanel, 3);
+
+
+                    StackPanel itemsChampionStackPanel = new StackPanel();
+                    TextBlock matchDurationChampionTextBlock = new TextBlock()
+                    {
+                        TextAlignment = TextAlignment.Center,
+                        Text = $"{match.Info.GameDuration}",
+                        FontSize = 20,
+                        FontFamily = new FontFamily("Assets/fonts/Inter/Inter-Medium.ttf#Inter")
+                    };
+
+                    Grid itemsChampionGrid = new Grid()
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center
+                    };
+                    ColumnDefinition itemColumn1 = new ColumnDefinition() {Width = new GridLength(20, GridUnitType.Pixel)};
+                    ColumnDefinition itemColumn2 = new ColumnDefinition() {Width = new GridLength(20, GridUnitType.Pixel)};
+                    ColumnDefinition itemColumn3 = new ColumnDefinition() {Width = new GridLength(20, GridUnitType.Pixel)};
+                    ColumnDefinition itemColumn4 = new ColumnDefinition() {Width = new GridLength(20, GridUnitType.Pixel)};
+
+                    itemsChampionGrid.ColumnDefinitions.Add(itemColumn1);
+                    itemsChampionGrid.ColumnDefinitions.Add(itemColumn2);
+                    itemsChampionGrid.ColumnDefinitions.Add(itemColumn3);
+                    itemsChampionGrid.ColumnDefinitions.Add(itemColumn4);
+
+                    RowDefinition itemRow1 = new RowDefinition() { Height = new GridLength(20, GridUnitType.Pixel) };
+                    RowDefinition itemRow2 = new RowDefinition() { Height = new GridLength(20, GridUnitType.Pixel) };
+
+                    Image itemImage0 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item0}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage0, 0);
+                    Grid.SetRow(itemImage0, 0);
+
+                    Image itemImage1 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item1}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage1, 1);
+                    Grid.SetRow(itemImage0, 0);
+
+                    Image itemImage2 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item2}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage2, 2);
+                    Grid.SetRow(itemImage2, 0);
+
+                    Image itemImage3 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item3}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage3, 3);
+                    Grid.SetRow(itemImage3, 0);
+
+                    Image itemImage4 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item4}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage4, 0);
+                    Grid.SetRow(itemImage4, 1);
+
+                    Image itemImage5 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item5}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage5, 1);
+                    Grid.SetRow(itemImage5, 1);
+
+                    Image itemImage6 = new Image()
+                    {
+                        Source = new BitmapImage(new Uri(
+                            $"http://ddragon.leagueoflegends.com/cdn/13.21.1/img/item/{participant.Item6}.png"))
+                    };
+
+                    Grid.SetColumn(itemImage6, 2);
+                    Grid.SetRow(itemImage6, 1);
+
+                    itemsChampionStackPanel.Children.Add(matchDurationChampionTextBlock);
+                    itemsChampionStackPanel.Children.Add(itemsChampionGrid);
+
+                    Grid.SetColumn(itemsChampionStackPanel, 3);
+                    Grid.SetRow(itemsChampionStackPanel, 2);
+                    Grid.SetColumnSpan(itemsChampionStackPanel, 3);
                 }
             }
         }
