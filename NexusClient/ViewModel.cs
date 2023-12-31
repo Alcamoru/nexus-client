1db137819adf7978efe76020e2b28f5a93ae67d0
Alexis <alcamoru@icloud.com>
2023-12-31 15:49:13 +0100

    Add LiveCharts to NexusClient
    Added ViewModel.cs file to implement LiveChartsCore in NexusClient for graphical representation of data. Modified NexusClient.csproj to include LiveChartsCore.SkiaSharpView package. In MatchDetail.xaml, added the ViewModel and a CartesianChart within a new grid to visualize the series data.

---
+++ NexusClient/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace NexusClient
+{
+    public class ViewModel
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<int>
+                {
+                    Values = new int[] { 4, 6, 5, 3, -3, -1, 2 }
+                },
+                new ColumnSeries<double>
+                {
+                    Values = new double[] { 2, 5, 4, -2, 4, -3, 5 }
+                }
+            };
+    }
+}
