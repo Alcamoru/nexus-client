a77c18315606c0818a2ad5a54c231070d2ec44b3
Alexis <alcamoru@icloud.com>
2024-01-05 07:43:47 +0100

    test

--- NexusClient/SummonerInfoPage.xaml.cs
+++ NexusClient/SummonerInfoPage.xaml.cs
@@ -26,6 +26,7 @@ using static NexusClient.UtilisMethods;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+
 namespace NexusClient;
 
 /// <summary>
