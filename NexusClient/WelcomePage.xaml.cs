a3dce2caf517792d514f442c9964aee047956e46
alcam <alcamoru@icloud.com>
2024-04-25 11:03:24 +0200

    Started to change welcome page

--- NexusClient/WelcomePage.xaml.cs
+++ NexusClient/WelcomePage.xaml.cs
@@ -5,12 +5,17 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.System;
+using Windows.UI;
 using Camille.Enums;
 using Camille.RiotGames;
 using Camille.RiotGames.SummonerV4;
+using Camille.RiotGames.Util;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -51,42 +56,6 @@ public sealed partial class WelcomePage : Page
     private PlatformRoute SummonerPlatformRoute { get; set; }
 
 
-    // Pour sélectionner la région
-    private void RegionListButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        RegionListView.Visibility = RegionListView.Visibility == Visibility.Collapsed
-            ? Visibility.Visible
-            : Visibility.Collapsed;
-    }
-
-    private void RegionListButton_OnLostFocus(object sender, RoutedEventArgs e)
-    {
-        RegionListView.Visibility = Visibility.Collapsed;
-    }
-
-    private void RegionListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        var item = (ListViewItem)e.AddedItems[0];
-        var itemContent = (TextBlock)item.Content;
-        RegionListButtonTextBlock.Text = itemContent.Text;
-        switch (itemContent.Text)
-        {
-            case "EUW":
-                SummonerRegionalRoute = RegionalRoute.EUROPE;
-                SummonerPlatformRoute = PlatformRoute.EUW1;
-                break;
-            case "NA":
-                SummonerRegionalRoute = RegionalRoute.AMERICAS;
-                SummonerPlatformRoute = PlatformRoute.NA1;
-                break;
-        }
-    }
-
-    private void SendSummonerNameButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        CheckIfExists();
-    }
-
     private void SummonerNameTextBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
     {
         if (e.Key == VirtualKey.Enter) CheckIfExists();
@@ -99,7 +68,7 @@ public sealed partial class WelcomePage : Page
         try
         {
             var lolAccount = (await Api.AccountV1()
-                .GetByRiotIdAsync(SummonerRegionalRoute, SummonerNameTextBox.Text, AccountTextBox.Text))!;
+                .GetByRiotIdAsync(SummonerRegionalRoute, SummonerNameAutoSuggestBox.Text, RiotIdAutoSuggestBox.Text))!;
             LolSummoner = await Api.SummonerV4().GetByPUUIDAsync(SummonerPlatformRoute, lolAccount.Puuid);
 
             if (LolSummoner is null) throw new ArgumentNullException();
@@ -114,7 +83,7 @@ public sealed partial class WelcomePage : Page
         catch (Exception e)
         {
             Debug.WriteLine(e);
-            if (e is ArgumentNullException) ErrorTextBlock.Text = "L'invocateur recherché est invalide";
+            if (e is ArgumentNullException || e is RiotResponseException) ErrorTextBlock.Text = "L'invocateur recherché est invalide";
 
             if (e is AggregateException) ErrorTextBlock.Text = "Le logiciel n'est pas connecté à Internet";
 
@@ -134,5 +103,29 @@ public sealed partial class WelcomePage : Page
         };
 
         Frame.Navigate(typeof(SummonerInfoPage), parametersList, new DrillInNavigationTransitionInfo());
+    }
+
+    private void MenuFlyoutItem_OnClick(object sender, RoutedEventArgs e)
+    {
+        MenuFlyoutItem buttonClicked = (MenuFlyoutItem)e.OriginalSource;
+        switch (buttonClicked.Text)
+        {
+            case "EUW":
+                SummonerRegionalRoute = RegionalRoute.EUROPE;
+                SummonerPlatformRoute = PlatformRoute.EUW1;
+                DropDownButton.Content = "EUW";
+                break;
+            case "NA":
+                SummonerRegionalRoute = RegionalRoute.AMERICAS;
+                SummonerPlatformRoute = PlatformRoute.NA1;
+                DropDownButton.Content = "NA";
+
+                break;
+        }
+    }
+
+    private void ButtonSearch_OnClick(object sender, RoutedEventArgs e)
+    {
+        CheckIfExists();
     }
 }
